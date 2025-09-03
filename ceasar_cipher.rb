f717e0fc76e36ba3cdc58fc98a4c2fa0c41c51f6
Dimitris Livanas <dimitris.livanas@ada.com>
2025-09-03 18:44:13 +0300

    feat: Implement Caesar Cipher with case and wrapping

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,16 +1,22 @@
 def caesar_cipher(string, shift_factor)
-  # the below taken from https://stackoverflow.com/questions/6343257/how-to-check-string-contains-special-character-in-ruby
-  special = "?<>',[]}{=-)(*&^%$#`~{}!"
-
-  result = string.chars.map do |char|
-    if char == " " || special.include?(char)
-      char
+  string.chars.map do |char|
+    new_ord = char.ord + shift_factor
+    if ("a".."z").cover?(char)
+      if new_ord > "z".ord
+        (new_ord - 26).chr
+      else
+        new_ord.chr
+      end
+    elsif ("A".."Z").cover?(char)
+      if new_ord > "Z".ord
+        (new_ord - 26).chr
+      else
+        new_ord.chr
+      end
     else
-      (char.ord + shift_factor).chr
+      char
     end
-  end
-
-  p result.join
+  end.join
 end
 
-caesar_cipher("What a string!", 5)
+p caesar_cipher("What a string!", 27)
