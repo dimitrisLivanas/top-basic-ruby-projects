31e0c62e8743581065c5c76b6c8145bd26b7085b
Dimitris Livanas <dimitris.livanas@ada.com>
2025-09-03 19:03:33 +0300

    refactor: use modulo for Caesar cipher alphabet wrapping

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,22 +1,16 @@
 def caesar_cipher(string, shift_factor)
   string.chars.map do |char|
-    new_ord = char.ord + shift_factor
     if ("a".."z").cover?(char)
-      if new_ord > "z".ord
-        (new_ord - 26).chr
-      else
-        new_ord.chr
-      end
+      ((char.ord - "a".ord + shift_factor) % 26 + "a".ord).chr
     elsif ("A".."Z").cover?(char)
-      if new_ord > "Z".ord
-        (new_ord - 26).chr
-      else
-        new_ord.chr
-      end
+      ((char.ord - "A".ord + shift_factor) % 26 + "A".ord).chr
     else
       char
     end
   end.join
 end
 
-p caesar_cipher("What a string!", 27)
+# Tests
+p caesar_cipher("What a string!", 5)
+p caesar_cipher("xyz", 3)
+p caesar_cipher("ABC", 27)
