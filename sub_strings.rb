de6b2328545c640a012adfc8df12fe5ccb97e23d
Dimitris Livanas <dimitris.livanas@ada.com>
2025-09-04 15:35:49 +0300

    feat: Add substrings file with examples/tests

---
+++ sub_strings.rb
@@ -0,0 +1,12 @@
+dictionary = ["below", "down", "go",
+              "going", "horn","how",
+              "howdy", "it", "i", "low",
+              "own", "part", "partner", "sit"]
+
+def substring
+
+end
+
+# Tests
+substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
