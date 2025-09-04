f8e0e21cd7840d33798c8bf208df2beacfffed84
Dimitris Livanas <dimitris.livanas@ada.com>
2025-09-04 16:14:24 +0300

    feat: Use scan to work with multiple words

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,10 +1,18 @@
-dictionary = ["below", "down", "go",
-              "going", "horn","how",
-              "howdy", "it", "i", "low",
-              "own", "part", "partner", "sit"]
+dictionary = [
+  "below", "down", "go",
+  "going", "horn", "how",
+  "howdy", "it", "i", "low",
+  "own", "part", "partner", "sit"
+]
 
-def substring
-
+def substrings(string, dictionary)
+  string = string.downcase
+  result = {}
+  dictionary.each do |word|
+    count = string.scan(word).length
+    result[word] = count if count > 0
+  end
+  p result
 end
 
 # Tests
