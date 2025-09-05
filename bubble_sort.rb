013660f64f6abd2d6d9b0ca420c6fce0af020580
Dimitris Livanas <dimitris.livanas@ada.com>
2025-09-05 09:45:31 +0300

    feat: Add and implement final Bubble Sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,16 @@
 def bubble_sort(array)
-  
+  loop do
+    swapped = false
+    (array.size - 1).times do |index|
+      if array[index] > array[index + 1]
+        array[index], array[index + 1] = array[index + 1], array[index]
+        swapped = true
+      end
+    end
+    break unless swapped
+  end
+  array
 end
 
 # Test
-bubble_sort([4, 3, 78, 2, 0, 2])
+p bubble_sort([4, 3, 78, 2, 0, 2])
